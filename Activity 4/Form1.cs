3f8281379b7d2f7c42d99c3756b41912fa636387
Akikti <99299991+Akikti@users.noreply.github.com>
2022-05-30 14:39:14 -0400

    Add files via upload

---
+++ Activity 4/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int totalSeconds, minutes, hours, days;
+            int secondsInDay = 86400 / 60;
+            int secondsInHour = 3600/60;
+            int secondsInMinute = 60 * 60;
+
+            int.TryParse(textBox1.Text, out totalSeconds);
+
+            if (totalSeconds < (secondsInDay))
+            {
+                days = totalSeconds / (secondsInDay);
+                totalSeconds = totalSeconds - (days / secondsInDay);
+                label2.Text = days.ToString() + "  Days";
+            }
+            else label2.Text = null;
+
+            if (totalSeconds < (secondsInHour))
+            {
+                hours = totalSeconds / (secondsInHour);
+                totalSeconds = totalSeconds - (hours / secondsInHour);
+                label2.Text = hours.ToString() + "  Hours";
+            }
+            else label2.Text = null;
+
+            if (totalSeconds < (secondsInMinute))
+            {
+                minutes = totalSeconds / secondsInMinute;
+                totalSeconds = totalSeconds - (minutes * secondsInMinute);
+                label2.Text = minutes.ToString() + "  Minutes";
+            }
+            else label2.Text = null;
+
+            if (totalSeconds <= 60)
+                label2.Text = totalSeconds.ToString() + "  Seconds";
+            else label2.Text = null;
+        }
+    }
+}
+        
+        
+  
---
+++ Activity 4/README.txt
@@ -0,0 +1,16 @@
+Project Title:
+	Activity 4
+	
+Build Status:
+	Complete
+	
+	
+Framework:
+	.NET
+
+Features:
+	textBox, button, labels
+	
+This form takes seconds and displays their number depending on the input.  If 
+more than 86,400 it will display the number of days, if more than 3600
+the number of hours, and if more than 60 the number of minutes.
