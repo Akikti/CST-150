35c1ee750ac8a66ab2f6d7aeefc095fd8e612ba9
Akikti <99299991+Akikti@users.noreply.github.com>
2022-06-20 16:31:54 -0400

    Add files via upload

---
+++ Activity 11/Dice Roller Activity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Roller
+{
+    public class Dice
+    {
+        private int numberOfSides;
+
+       
+        private static Random rnd = new Random();
+
+        public Dice(int numberOfSides)
+        {
+
+            if (numberOfSides < 4 || numberOfSides > 20)
+            {
+                throw new ArgumentOutOfRangeException("Should be in range [4, 20] !");
+            }
+
+            this.numberOfSides = numberOfSides;
+        }
+
+        public int rollDie()
+        {
+            return rnd.Next(1, numberOfSides + 1);
+        }
+
+    }
+
+    public class DieRoll
+    {
+        public static void Main()
+        {
+            dice1 = new Dice(6);
+            dice2 = new Dice(6);
+
+            int dice1Roll, dice2Roll;
+            int numberOfRolls = 0;
+
+            do
+            {
+
+                dice1Roll = dice1Roll.rollDie();
+                dice2Roll = dice2Roll.rollDie();
+
+                Console.WriteLine("Rolled [Dice 1: " + dice1Roll + ", Dice 2: " + dice2Roll + "]");
+
+                ++numberOfRolls;
+
+            } while (dice1Roll != 1 || dice2Roll != 1);
+
+            Console.WriteLine("\nIt took " + numberOfRolls + " rolls to get snake eyes!");
+        }
+    }
+}
---
+++ Activity 11/README.txt
@@ -0,0 +1,12 @@
+Project Title:
+	Activity 11
+	
+Build Status:
+	Complete
+	
+Github Repository:
+	https://github.com/Akikti/CST-150/upload/main/Activity%2011
+
+Framework: .NET
+
+This programs allows for a random role of the dice.
